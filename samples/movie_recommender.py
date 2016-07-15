fd274a2b43d9ca4d1dcf9fcc042e9a3c8fdf7f02
Gopal Erinjippurath <gopal.e@gmail.com>
2016-07-15 21:45:31 +0530

    Adding implicit feedbacck trial

--- samples/movie_recommender.py
+++ samples/movie_recommender.py
@@ -26,22 +26,35 @@ rank = 50
 numIterations = 10 #for some reason crashes when this number is increased beyond 10
 reg_param = 0.01
 model = ALS.train(ratings, rank, numIterations)
+alpha = 0.01
+lambdax = 0.01
+model2 = ALS.trainImplicit(ratings, rank, numIterations, alpha)
 
 # Evaluate the model on training data
 testdata = ratings.map(lambda p: (p[0], p[1]))
 # predictions in form {(user, item), prediction}
 predictions = model.predictAll(testdata).map(lambda r: ((r[0], r[1]), r[2]))
-# joining RDDDs for predictions with original score resulting in {(user, item), (score, prediction)}
+# joining RDDs for predictions with original score resulting in {(user, item), (score, prediction)}
 ratesAndPreds = ratings.map(lambda r: ((r[0], r[1]), r[2])).join(predictions)
 # compute E((score-prediction)^2)
 MSE = ratesAndPreds.map(lambda r: (r[1][0] - r[1][1])**2).mean()
 
 results = ratesAndPreds.collect()
 
+# predictions in form {(user, item), prediction}
+predictions = model2.predictAll(testdata).map(lambda r: ((r[0], r[1]), r[2]))
+# joining RDDs for predictions with original score resulting in {(user, item), (score, prediction)}
+ratesAndPreds = ratings.map(lambda r: ((r[0], r[1]), r[2])).join(predictions)
+# compute E((score-prediction)^2)
+MSE2 = ratesAndPreds.map(lambda r: (r[1][0] - r[1][1])**2).mean()
+
+results2 = ratesAndPreds.collect()
+
 print "Model user features : ", model.userFeatures
-for item in results:
+for item in results[0:20]:
 	print item[0][0], "\t", item[0][1], "\t", item[1][0], "\t", item[1][1] 
-print("Mean Squared Error : " + str(MSE))
+print("Mean Squared Error Explicit: " + str(MSE))
+print("Mean Squared Error Implicit: " + str(MSE2))
 
 userID = 771
 numProducts = 15
