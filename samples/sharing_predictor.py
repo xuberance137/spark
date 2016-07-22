2d255e61dea9d9ba542c1db841b0abaf27c00224
Gopal Erinjippurath <gopal.e@gmail.com>
2016-07-22 13:18:28 +0530

    Adding prediction models with starter params

--- samples/sharing_predictor.py
+++ samples/sharing_predictor.py
@@ -1,9 +1,10 @@
 # Sample Usage:
-# $./bin/spark-submit --driver-memory 4g ./samples/movie_recommender.py 
+# $./bin/spark-submit ./samples/sharing_predictor.py 
 
 from pyspark import SparkConf
 from pyspark import SparkContext
-from pyspark.mllib.regression import LabeledPoint
+from pyspark.mllib.regression import LabeledPoint, LinearRegressionWithSGD
+from pyspark.mllib.tree import DecisionTree
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
@@ -14,6 +15,7 @@ sc.setLogLevel("ERROR")
 def get_mapping(rdd, idx):
 	return rdd.map(lambda fields: fields[idx]).distinct().zipWithIndex().collectAsMap()
 
+#mapping categorialfeatures to binary vectors
 def extract_features(record):
 	cat_vec = np.zeros(cat_len)
 	i = 0
@@ -31,6 +33,10 @@ def extract_features(record):
 def extract_label(record):
 	return float(record[-1])
 
+def extract_features_dt(record):
+	return np.array(map(float, record[2:14]))
+
+
 PATH = "/Users/gopal/projects/learning/spark/spark-1.6.1-bin-hadoop2.6"
 DATAFILEPATH = PATH + "/data/Bike-Sharing-Dataset/hour_noheader.csv"
 
@@ -40,25 +46,38 @@ records = rawData.map(lambda x: x.split(","))
 records.cache()
 first = records.first()
 
-#new_rdd = get_mapping(records, 2)
-
 #run get_mapping function over categorical features
 mappings = [get_mapping(records, index) for index in range(2, 10)]
-print mappings
-
 #length of categorial features mapped to binary vectors
 cat_len = sum(map(len, mappings))
-#length of numeric features
-print records.first()[10:14]
+# length of numeric features
+# print records.first()[10:14]
 num_len = len(records.first()[10:14])
+# length of linear regressor feature vector
 total_len =  cat_len + num_len
 
 data = records.map(lambda datapoint: LabeledPoint(extract_label(datapoint), extract_features(datapoint)))
+data_dt = records.map(lambda datapoint: LabeledPoint(extract_label(datapoint), extract_features_dt(datapoint)))
 
-sample = data.first()
-print "Label : ", str(sample.label)
-print "Features : ", str(sample.features)
+# sample = data.first()
+# print "Label : ", str(sample.label)
+# print "Features : ", str(sample.features)
 
+linear_model = LinearRegressionWithSGD.train(data, iterations=10, step=0.1, intercept=False)
+true_vs_predicted = data.map(lambda p: (p.label, linear_model.predict(p.features)))
+
+dt_model = DecisionTree.trainRegressor(data_dt, {})
+preds = dt_model.predict(data_dt.map(lambda p: p.features))
+actual = data_dt.map(lambda p: p.label)
+true_vs_predicted_dt = actual.zip(preds)
+
+print "Linear Model predictions : "
+for item in true_vs_predicted.take(10):
+	print item
+
+print "Decision Tree predictions : "
+for item in true_vs_predicted_dt.take(10):
+	print item
 
 
 
