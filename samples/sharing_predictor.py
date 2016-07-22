3ca80803bece2dec3dbc955d2aea4b2fea84342c
Gopal Erinjippurath <gopal.e@gmail.com>
2016-07-22 10:47:47 +0530

    Creating Feature vectors

--- samples/sharing_predictor.py
+++ samples/sharing_predictor.py
@@ -4,21 +4,32 @@
 from pyspark import SparkConf
 from pyspark import SparkContext
 from pyspark.mllib.regression import LabeledPoint
-from pyspark.mllib.linalg import Vectors
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-
-from pyspark.mllib.classification import LogisticRegressionWithSGD
-from pyspark.mllib.classification import SVMWithSGD
-from pyspark.mllib.classification import NaiveBayes
-from pyspark.mllib.tree import DecisionTree
 
 sc = SparkContext(appName="sharing_predictor")
 sc.setLogLevel("ERROR")
 
 def get_mapping(rdd, idx):
 	return rdd.map(lambda fields: fields[idx]).distinct().zipWithIndex().collectAsMap()
+
+def extract_features(record):
+	cat_vec = np.zeros(cat_len)
+	i = 0
+	step = 0
+	for field in record[2:9]:
+		m = mappings[i]
+		index = m[field]
+		cat_vec[index + step] = 1
+		i = i + 1
+		step = step + len(m)
+	num_vec = np.array([float(field) for field in record[10:14]])
+
+	return np.concatenate((cat_vec, num_vec))
+
+def extract_label(record):
+	return float(record[-1])
 
 PATH = "/Users/gopal/projects/learning/spark/spark-1.6.1-bin-hadoop2.6"
 DATAFILEPATH = PATH + "/data/Bike-Sharing-Dataset/hour_noheader.csv"
@@ -41,5 +52,17 @@ cat_len = sum(map(len, mappings))
 print records.first()[10:14]
 num_len = len(records.first()[10:14])
 total_len =  cat_len + num_len
+
+data = records.map(lambda datapoint: LabeledPoint(extract_label(datapoint), extract_features(datapoint)))
+
+sample = data.first()
+print "Label : ", str(sample.label)
+print "Features : ", str(sample.features)
+
+
+
+
+
+
 
 
